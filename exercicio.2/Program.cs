55af26148978bdd5357e59620e324fc9b267f249
Gustavo0201 <gustavo0201tartari@@gmail.com>
2022-03-02 22:25:29 -0300

    exercicio-3 aula

---
+++ exercicio.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor em U$: ");
+            string valor = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valor, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido / valorCambio;
+
+            Console.WriteLine($"Valor convertido:  {valorCambioConvertido}");
+            Console.ReadLine();
+        }
+    }
+}
